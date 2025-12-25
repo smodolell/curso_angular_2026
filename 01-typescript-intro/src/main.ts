e210fcaff2135fccf3253a1d358d425ecf908646
Sergio Modolell <sergio.modolell@gmail.com>
2025-12-25 03:59:20 -0300

    01-basic-types

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,5 +1,7 @@
 import './style.css'
 
+import './topics/01-basic-types'
+
 const app = document.querySelector<HTMLDivElement>('#app');
 
 app.innerHTML="Sergio Modolell";
---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,14 @@
+const name = "Sergio";
+let hpPoints:number | string = 95;
+const isAlive:boolean = true;
+
+hpPoints ="FULL";
+hpPoints =100;
+
+console.log({
+name,
+hpPoints,
+isAlive
+})
+
+export {};

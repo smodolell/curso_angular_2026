c1efbc35c29e2c1b6a158cc7e6d55c4a7ef591f5
Sergio Modolell <sergio.modolell@gmail.com>
2025-12-25 18:55:54 -0300

    08-classes_2

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -10,6 +10,16 @@
 export class Persona {
   constructor(public name: string, public address: string = "No Address") {}
 }
+class Hero extends Persona{
+    constructor(
+        public alterEgo:string,
+        public age:number,
+        public realName:string
+    ){
 
-const ironman = new Persona("Iroman", "Salta");
+        super(realName,'New York');
+    } 
+}
+const ironman = new Hero("Iroman",45, "Tony");
+
 console.log(ironman);

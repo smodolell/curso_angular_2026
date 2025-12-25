edf9a909043d3f6133235e2566eae983e519dc48
Sergio Modolell <sergio.modolell@gmail.com>
2025-12-25 05:43:23 -0300

    03-functions_1

--- 01-typescript-intro/src/topics/03-functions.ts
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -9,10 +9,31 @@ const addNumbersArrow = (a: number, b: number): string => {
 function multiply(firstNumber:number,secondNumber?:number,base:number = 2){
     return firstNumber * base;
 }
-const result: number = addNumbers(1, 2);
-const result2: string = addNumbersArrow(1, 2);
-const multiplyResult :number = multiply(5);
+// const result: number = addNumbers(1, 2);
+// const result2: string = addNumbersArrow(1, 2);
+// const multiplyResult :number = multiply(5);
 
-console.log({ result, result2,multiplyResult });
+// console.log({ result, result2,multiplyResult });
+interface Character{
+    name:string;
+    hp:number;
+    showHp:()=> void;
+}
 
+const healCharater = (character:Character,amount:number) =>{
+    character.hp +=amount;
+}
+
+const strider :Character ={
+    name:"Strider",
+    hp:50,
+    showHp() {
+        console.log(`Puntos de vida:${this.hp}` );
+    }
+}
+
+healCharater(strider,30);
+healCharater(strider,50);
+ 
+strider.showHp();
 export {};

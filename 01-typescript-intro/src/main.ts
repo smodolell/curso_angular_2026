91d8da6f62da094b2493ac95b1a0b4550e5e6c91
Sergio Modolell <sergio.modolell@gmail.com>
2025-12-25 04:33:14 -0300

    03-functions

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,7 +1,9 @@
 import './style.css'
 
 // import './topics/01-basic-types'
-import './topics/02-object-interface'
+// import './topics/02-object-interface'
+import './topics/03-functions'
+
 const app = document.querySelector<HTMLDivElement>('#app');
 
 app.innerHTML="Sergio Modolell";

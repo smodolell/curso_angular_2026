23300d2638fe765a7ca910f694e33abd6b82f366
Sergio Modolell <sergio.modolell@gmail.com>
2025-12-25 18:49:06 -0300

    08-classes_1

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -1,11 +1,15 @@
+// export class Persona {
+//   public name: string;
+//   public address: string;
+
+//   constructor(name:string ,address:string) {
+//     this.name = name;
+//     this.address = address;
+//   }
+// }
 export class Persona {
-  public name: string;
-  public address: string;
-  constructor() {
-    this.name = "Sergio Modolell";
-    this.address = "Metan";
-  }
+  constructor(public name: string, public address: string = "No Address") {}
 }
 
-const ironman = new Persona()
+const ironman = new Persona("Iroman", "Salta");
 console.log(ironman);

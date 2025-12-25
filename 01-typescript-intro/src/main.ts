9d3d213d8dc5b7f9dea62f2cf846a494b44d2f26
Sergio Modolell <sergio.modolell@gmail.com>
2025-12-25 04:13:10 -0300

    02-object-interface

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,7 +1,7 @@
 import './style.css'
 
-import './topics/01-basic-types'
-
+// import './topics/01-basic-types'
+import './topics/02-object-interface'
 const app = document.querySelector<HTMLDivElement>('#app');
 
 app.innerHTML="Sergio Modolell";

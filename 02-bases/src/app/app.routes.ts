a1d7ea1f2d7d1f53e9373a10408e30a32e74ef78
Sergio Modolell <sergio.modolell@gmail.com>
2025-12-26 04:01:48 -0300

    50. Tarea - Preparación de página

--- 02-bases/src/app/app.routes.ts
+++ 02-bases/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponet } from './pages/counter/counter-page.component';
 import { HeroPageComponent } from './pages/hero/hero-page.component';
+import { DragonballPage } from './pages/dragonball-page/dragonball-page';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,10 @@ export const routes: Routes = [
   {
     path:"hero",
     component: HeroPageComponent
+  },
+  {
+    path:"dragonball",
+    component: DragonballPage
   },
   {
     path:"**",
---
+++ 02-bases/src/app/pages/dragonball-page/dragonball-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './dragonball-page.html',
+})
+export class DragonballPage {
+
+}

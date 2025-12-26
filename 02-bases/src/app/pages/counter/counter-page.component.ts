b79dc1f970848215aba63544900ecfc511ae21e3
Sergio Modolell <sergio.modolell@gmail.com>
2025-12-25 21:26:56 -0300

    36. Counter Page - Componente 2

--- 02-bases/src/app/pages/counter/counter-page.component.ts
+++ 02-bases/src/app/pages/counter/counter-page.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  template: `
-    <h1>Counter:{{ counter }}</h1>
-    <button (click)="incrementBy(1)">Incrementar Valor</button>
-  `,
+  templateUrl: './counter-page.component.html',
+  styleUrl:'./counter-page.component.css'
 })
 export class CounterPageComponet {
   counter: number = 10;
   incrementBy(value: number) {
     this.counter += value;
+    if (this.counter < 0) {
+      this.counter = 0;
+    }
+  }
+  reset() {
+    this.counter = 10;
   }
 }

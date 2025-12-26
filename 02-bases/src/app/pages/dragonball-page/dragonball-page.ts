74c9c9a82fd5fd7144b835dff850d062965d1ad3
Sergio Modolell <sergio.modolell@gmail.com>
2025-12-26 04:13:36 -0300

    51. @for - Realizar iteraciones de elementos

--- 02-bases/src/app/pages/dragonball-page/dragonball-page.ts
+++ 02-bases/src/app/pages/dragonball-page/dragonball-page.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   imports: [],
   templateUrl: './dragonball-page.html',
 })
 export class DragonballPage {
+  characters = signal<Character[]>([
+    {
+      id: 1,
+      name: 'Goku',
+      power: 90000,
+    },
+    {
+      id: 2,
+      name: 'Pikoro',
+      power: 80000,
+    },
+  ]);
+}
 
+export interface Character {
+  id: number;
+  name: string;
+  power: number;
 }

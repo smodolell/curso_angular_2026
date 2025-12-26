2d6deb1f48369e015fd91ac29878a660a701b5e1
Sergio Modolell <sergio.modolell@gmail.com>
2025-12-25 21:36:38 -0300

    38. Señales

--- 02-bases/src/app/pages/counter/counter-page.component.ts
+++ 02-bases/src/app/pages/counter/counter-page.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   templateUrl: './counter-page.component.html',
-  styleUrl:'./counter-page.component.css'
+  styleUrl: './counter-page.component.css',
 })
 export class CounterPageComponet {
   counter: number = 10;
+  counterSingnal = signal(10);
+
   incrementBy(value: number) {
     this.counter += value;
+    this.counterSingnal.update((current)=>{
+      return current +  value;
+
+    });
     if (this.counter < 0) {
       this.counter = 0;
     }
   }
   reset() {
-    this.counter = 10;
+    this.counter = 0;
+    this.counterSingnal.set(0);
   }
 }

5ba8451cf8091f3891a8a24ee346fe7788e2c458
Sergio Modolell <sergio.modolell@gmail.com>
2025-12-26 03:10:44 -0300

    42. Señales computadas - Readonly Signals

--- 02-bases/src/app/pages/counter/hero/hero-page.component.ts
+++ 02-bases/src/app/pages/counter/hero/hero-page.component.ts
@@ -1,15 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   templateUrl: './hero-page.component.html',
+  imports:[UpperCasePipe]
 })
 export class HeroPageComponent {
   name = signal('Ironman');
   age = signal(45);
+  heroDescription = computed(() => {
+    const desctiption = `${this.name()} - ${this.age()}`;
+    return desctiption;
+  });
 
-  getHeroDescription(): string {
-    return `${this.name()} - ${this.age()}`;
-  }
+  capitalizarName = computed(() => {
+    return this.name().toUpperCase();
+  });
   changeHero(): void {
     this.name.set('Spiderman');
     this.age.set(22);

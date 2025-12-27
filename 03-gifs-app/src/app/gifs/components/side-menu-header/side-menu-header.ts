733d75f74e2d8177e7a6a0ed271ea807dc8c31f9
Sergio Modolell <sergio.modolell@gmail.com>
2025-12-27 05:42:09 -0300

    71. Componentes para el menú lateral

---
+++ 03-gifs-app/src/app/gifs/components/side-menu-header/side-menu-header.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-menu-header',
+  imports: [],
+  templateUrl: './side-menu-header.html'
+})
+export class SideMenuHeader { }
--- 03-gifs-app/src/app/gifs/pages/dashboard-page/dashboard-page.ts
+++ 03-gifs-app/src/app/gifs/pages/dashboard-page/dashboard-page.ts
@@ -1,9 +1,10 @@
 import {  Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideMenu } from "../../components/side-menu/side-menu";
 
 @Component({
   selector: 'gifs-dashboard-page',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideMenu],
   templateUrl: './dashboard-page.html',
 })
 export default class DashboardPage { }

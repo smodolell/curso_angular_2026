eb4a949befe833e7672e234297a17d3b8f4b546f
Sergio Modolell <sergio.modolell@gmail.com>
2025-12-27 00:03:49 -0300

    59. Output - Emitir valores

--- 02-bases/src/app/components/dragonball/character-add/character-add.ts
+++ 02-bases/src/app/components/dragonball/character-add/character-add.ts
@@ -1,32 +1,32 @@
-import { Component,signal } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 import { Character } from '../../../interfaces/character';
 
 @Component({
   selector: 'dragonball-character-add',
   imports: [],
   templateUrl: './character-add.html',
-
 })
 export class CharacterAdd {
-
   name = signal('Gohan');
   power = signal(100);
 
-    addCharacter() {
+  newCharacter = output<Character>();
+
+  addCharacter() {
     console.log('Name:', this.name());
     console.log('Power:', this.power());
 
     if (!this.name() || !this.power() || this.power() < 0) {
       return;
     }
-    const newCharacter: Character = {
-      // id: this.characters().length + 1,
-      id:10,
+    const result: Character = {
+      id: Math.floor(Math.random() * 1000),
       name: this.name(),
       power: this.power(),
     };
+    this.newCharacter.emit(result);
 
-    console.log(newCharacter);
+    console.log(result);
     // this.characters.update((list) => [...list, newCharacter]);
 
     this.resetField();
@@ -36,4 +36,4 @@ export class CharacterAdd {
     this.name.set('');
     this.power.set(100);
   }
- }
+}
--- 02-bases/src/app/pages/dragonball-super/dragonball-super-page.ts
+++ 02-bases/src/app/pages/dragonball-super/dragonball-super-page.ts
@@ -1,22 +1,21 @@
-import {  Component, signal } from '@angular/core';
-import { CraracterList } from "../../components/dragonball/craracter-list/craracter-list";
+import { Component, signal } from '@angular/core';
+import { CraracterList } from '../../components/dragonball/craracter-list/craracter-list';
 import { Character } from '../../interfaces/character';
-import { CharacterAdd } from "../../components/dragonball/character-add/character-add";
+import { CharacterAdd } from '../../components/dragonball/character-add/character-add';
 
 @Component({
   imports: [CraracterList, CharacterAdd],
-  templateUrl:"./dragonball-super-page.html"
+  templateUrl: './dragonball-super-page.html',
 })
 export class DragonballSuperPage {
-
-
-
- characters = signal<Character[]>([
+  characters = signal<Character[]>([
     {
       id: 1,
       name: 'Goku',
       power: 9001,
-    }
+    },
   ]);
-
+  addCharacter(newCharacter: Character) {
+    this.characters.update((list) => [...list, newCharacter]);
+  }
 }

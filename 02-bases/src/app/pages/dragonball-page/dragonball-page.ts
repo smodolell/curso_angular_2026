e34659540669fef76efd3c4287f9347eb24a4fdd
Sergio Modolell <sergio.modolell@gmail.com>
2025-12-26 22:38:29 -0300

    55. Solución a la tarea

--- 02-bases/src/app/pages/dragonball-page/dragonball-page.ts
+++ 02-bases/src/app/pages/dragonball-page/dragonball-page.ts
@@ -6,6 +6,9 @@ import { Component, computed, signal } from '@angular/core';
   templateUrl: './dragonball-page.html',
 })
 export class DragonballPage {
+  name = signal('Gohan');
+  power = signal(100);
+
   characters = signal<Character[]>([
     {
       id: 1,
@@ -16,23 +19,46 @@ export class DragonballPage {
       id: 2,
       name: 'Pikoro',
       power: 8000,
-    },{
+    },
+    {
       id: 3,
       name: 'Vegeta',
-      power: 3000
+      power: 3000,
     },
     {
       id: 4,
       name: 'Yamcha',
-      power: 500
-    }
+      power: 500,
+    },
   ]);
+  addCharacter() {
+    console.log('Name:', this.name());
+    console.log('Power:', this.power());
 
+    if (!this.name() || !this.power() || this.power() < 0) {
+      return;
+    }
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+
+    // this.characters().push(newCharacter);
+    this.characters.update((list) => [...list, newCharacter]);
+
+    this.resetField();
+  }
   // powerClasses = computed(()=>{
   //   return {
   //     'text-danger':true
   //   }
   // });
+
+  resetField(): void {
+    this.name.set('');
+    this.power.set(100);
+  }
 }
 
 export interface Character {

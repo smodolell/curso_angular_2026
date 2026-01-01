fa4bbc9933de194a62ae7ea154d84104ed9f0216
Sergio Modolell <sergio.modolell@gmail.com>
2026-01-01 11:06:24 -0300

    98. Determinar fin de scroll

--- 03-gifs-app/src/app/gifs/pages/trending-page/trending-page.ts
+++ 03-gifs-app/src/app/gifs/pages/trending-page/trending-page.ts
@@ -9,13 +9,21 @@ import { GifsService } from '../../services/gifs.service';
 export default class TrendingPage {
   gifService = inject(GifsService);
 
-  scrollDivRef = viewChild<ElementRef>('divGroup');
-
+  scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('divGroup');
 
   onScroll(event: Event) {
     const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
+    const scrollTop = scrollDiv.scrollTop;
+    const clientHeight = scrollDiv.clientHeight;
+    const scrollHeight = scrollDiv.scrollHeight;
+    const IsAtButton = scrollTop + clientHeight +300 >= scrollHeight;
 
-    console.log('scroll event', scrollDiv);
+    if(IsAtButton){
+
+
+    }
+    console.log('IsAtButton', IsAtButton);
 
   }
 }

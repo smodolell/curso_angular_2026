6ab13b58e980fe48f1b990b23026f04ef3b8d2d9
Sergio Modolell <sergio.modolell@gmail.com>
2026-01-01 13:04:41 -0300

    108. Layout Components

--- 04-country-app/src/app/country/country.routers.ts
+++ 04-country-app/src/app/country/country.routers.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { ByCapitalPage } from './pages/by-capital-page/by-capital-page';
+import { CountryLayout } from './layouts/country-layout/country-layout';
 
 export const countryRoutes: Routes = [
-  { path: '', component: ByCapitalPage},
-  { path: '**', redirectTo: '' },
+  {
+    path: '',
+    component: CountryLayout,
+    children: [
+      { path: 'by-capital', component: ByCapitalPage },
+      { path: '**', redirectTo: 'by-capital' },
+    ],
+  },
 ];
 
 export default countryRoutes;
-
---
+++ 04-country-app/src/app/country/layouts/country-layout/country-layout.ts
@@ -0,0 +1,10 @@
+import {  Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-country-layout',
+  imports: [RouterOutlet],
+  templateUrl: './country-layout.html',
+
+})
+export class CountryLayout { }

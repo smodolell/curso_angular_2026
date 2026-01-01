435f9a6ab0ee6989877f4d0455ceb1fc74246cff
Sergio Modolell <sergio.modolell@gmail.com>
2026-01-01 13:21:11 -0300

    109. DaisyUI - Componentes

--- 04-country-app/src/app/shared/pages/home-page/home-page.ts
+++ 04-country-app/src/app/shared/pages/home-page/home-page.ts
@@ -1,8 +1,10 @@
 import {  Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home-page.html',
 })
 export class HomePage { }

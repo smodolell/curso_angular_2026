72f11d757d0c5be00bedd5e1defd472a3ff2822e
Sergio Modolell <sergio.modolell@gmail.com>
2026-01-02 23:50:42 -0300

    119. CountryService - Interfaces

--- 04-country-app/src/app/country/pages/by-capital-page/by-capital-page.ts
+++ 04-country-app/src/app/country/pages/by-capital-page/by-capital-page.ts
@@ -1,12 +1,20 @@
-import {  Component } from '@angular/core';
-import { CountrySearchInput } from "../../components/country-search-input/country-search-input";
-import { CountryList } from "../../components/country-list/country-list";
+import { Component, inject } from '@angular/core';
+import { CountrySearchInput } from '../../components/country-search-input/country-search-input';
+import { CountryList } from '../../components/country-list/country-list';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-capital-page',
   imports: [CountrySearchInput, CountryList],
-  templateUrl: './by-capital-page.html'
+  templateUrl: './by-capital-page.html',
 })
 export class ByCapitalPage {
+  countryService = inject(CountryService);
 
- }
+onSearch(query?: string): void {
+    console.log({ term: query });
+    this.countryService.searchByCapital(query || '').subscribe((countries) => {
+      console.log(countries);
+    });
+  }
+}

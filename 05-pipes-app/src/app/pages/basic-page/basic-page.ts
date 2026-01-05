6247b2b5910be9a8f71814a18949f983666bb238
Sergio Modolell <sergio.modolell@gmail.com>
2026-01-05 20:47:11 -0300

    153. Date Pipe

--- 05-pipes-app/src/app/pages/basic-page/basic-page.ts
+++ 05-pipes-app/src/app/pages/basic-page/basic-page.ts
@@ -1,13 +1,24 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe,UpperCasePipe,TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.html',
 })
 export default class BasicPage {
   nameLower = signal('sergio');
   nameUpper = signal('SERGIO');
   fullName = signal('SERGiO MoDoleLL');
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanup) => {
+    const inteval = setInterval(() => {
+      this.customDate.set(new Date());
+      console.log('tick');
+    }, 1000);
+    onCleanup: () => {
+      clearInterval(inteval);
+    };
+  });
 }

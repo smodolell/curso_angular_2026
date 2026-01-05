7e479e6a26297824b066af71334798c9d1a23d3d
Sergio Modolell <sergio.modolell@gmail.com>
2026-01-05 18:42:23 -0300

    149. Inicio de proyecto

---
+++ 05-pipes-app/src/app/app.routes.ts
@@ -0,0 +1,28 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'basic',
+    title: 'Pipes Básicos',
+    loadComponent: () => import('./pages/basic-page/basic-page'),
+  },
+  {
+    path: 'numbers',
+    title: 'Numbers Pipes',
+    loadComponent: () => import('./pages/numbers-page/numbers-page'),
+  },
+  {
+    path: 'uncommon',
+    title: 'Pipes no tan comumes',
+    loadComponent: () => import('./pages/uncommon-page/uncommon-page'),
+  },
+  {
+    path: 'custom',
+    title: 'Pipes Personalizados',
+    loadComponent: () => import('./pages/custom-page/custom-page'),
+  },
+  {
+    path: '**',
+    redirectTo: 'basic',
+  },
+];
---
+++ 05-pipes-app/src/app/pages/numbers-page/numbers-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-numbers-page',
+  imports: [],
+  templateUrl: './numbers-page.html',
+})
+export default class NumbersPage { }

f692c6fc129d75ed401ad71e48470aea3209d369
Sergio Modolell <sergio.modolell@gmail.com>
2026-01-05 19:21:14 -0300

    151. UpperCase, LowerCase y TitleCase Pipes

--- 05-pipes-app/src/app/pages/basic-page/basic-page.ts
+++ 05-pipes-app/src/app/pages/basic-page/basic-page.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [LowerCasePipe,UpperCasePipe,TitleCasePipe],
   templateUrl: './basic-page.html',
 })
-export default class BasicPage { }
+export default class BasicPage {
+  nameLower = signal('sergio');
+  nameUpper = signal('SERGIO');
+  fullName = signal('SERGiO MoDoleLL');
+}

d01c8bb471ade85b65858e52633068802947d9ab
Sergio Modolell <sergio.modolell@gmail.com>
2026-01-06 19:14:27 -0300

    179. Inicio de aplicación - FormulariosApp

---
+++ 06-reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-page',
+  imports: [],
+  templateUrl: './dynamic-page.html',
+})
+export class DynamicPage { }
---
+++ 06-reactive-forms-app/src/app/reactive/pages/switches-page/switches-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-switches-page',
+  imports: [],
+  templateUrl: './switches-page.html',
+})
+export class SwitchesPage { }

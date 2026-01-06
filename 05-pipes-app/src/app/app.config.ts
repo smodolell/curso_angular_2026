12583b4297b1847a7aed8f21273f95b412e8edf8
Sergio Modolell <sergio.modolell@gmail.com>
2026-01-05 21:01:14 -0300

    154. Cambiar de idiomas

--- 05-pipes-app/src/app/app.config.ts
+++ 05-pipes-app/src/app/app.config.ts
@@ -1,11 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { registerInjectable } from '@angular/core/primitives/di';
+import { registerLocaleData } from '@angular/common';
+
+import localEs from '@angular/common/locales/es-MX'
+import localFr from '@angular/common/locales/fr'
+
+registerLocaleData(localEs,'es');
+registerLocaleData(localFr,'fr');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    {
+      provide:LOCALE_ID,
+      useValue:'es'
+    }
   ]
 };

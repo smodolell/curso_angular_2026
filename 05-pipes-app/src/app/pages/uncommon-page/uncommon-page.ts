cafca66e08048f35c7cc52b0834b8513eaa82a55
Sergio Modolell <sergio.modolell@gmail.com>
2026-01-06 13:55:52 -0300

    157. I18nSelectPipe

--- 05-pipes-app/src/app/pages/uncommon-page/uncommon-page.ts
+++ 05-pipes-app/src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
-import { Card } from "../../components/card/card";
-
+import { Component, signal } from '@angular/core';
+import { Card } from '../../components/card/card';
+import { I18nSelectPipe } from '@angular/common';
+const client1 = {
+  name: 'Sergio',
+  gender: 'male',
+  age: 41,
+  address: '9 de julio 4545',
+};
+const client2 = {
+  name: 'Maria',
+  gender: 'female',
+  age: 45,
+  address: 'Toronto, Candad',
+};
 @Component({
   selector: 'app-uncommon-page',
-  imports: [Card],
+  imports: [Card,I18nSelectPipe],
   templateUrl: './uncommon-page.html',
 })
-export default class UncommonPage { }
+export default class UncommonPage {
+  client = signal(client1);
+
+  invitationMap ={
+    male:'invitarlo',
+    female:'invitarla'
+  }
+  changeClient() {
+    if (this.client() === client1) {
+      this.client.set(client2);
+    } else {
+      this.client.set(client1);
+    }
+  }
+}

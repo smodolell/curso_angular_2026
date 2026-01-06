f26e97c88f5b2ab4b1c550efb9f78242fc38f027
Sergio Modolell <sergio.modolell@gmail.com>
2026-01-06 16:11:11 -0300

    169. Pipe personalizado - ToggleCase

--- 05-pipes-app/src/app/pages/custom-page/custom-page.ts
+++ 05-pipes-app/src/app/pages/custom-page/custom-page.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [],
+  imports: [ToggleCasePipe],
   templateUrl: './custom-page.html',
 })
-export default class CustomPage { }
+export default class CustomPage {
+  name = signal<string>('Sergio');
+
+  upperCase = signal<boolean>(false);
+
+  changeUpperCase() {
+    if (this.upperCase()) {
+      this.upperCase.set(false);
+    } else {
+      this.upperCase.set(true);
+    }
+  }
+}

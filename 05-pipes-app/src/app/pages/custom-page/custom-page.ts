2090139cbaf70a399a6e07b61edcaccb5c994779
Sergio Modolell <sergio.modolell@gmail.com>
2026-01-06 17:27:09 -0300

    172. Pipe personalizado - HeroTextColor

--- 05-pipes-app/src/app/pages/custom-page/custom-page.ts
+++ 05-pipes-app/src/app/pages/custom-page/custom-page.ts
@@ -3,11 +3,13 @@ import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../data/heroes.data';
 import { CanFlyPipe } from '../../pipes/can-fly.pipe';
 import { HeroColorPipe } from '../../pipes/hero-color.pipe';
+import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
+import { TitleCasePipe } from '@angular/common';
+import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [
-    ToggleCasePipe,CanFlyPipe,HeroColorPipe],
+  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, TitleCasePipe,HeroCreatorPipe],
   templateUrl: './custom-page.html',
 })
 export default class CustomPage {
@@ -15,7 +17,6 @@ export default class CustomPage {
   upperCase = signal<boolean>(false);
 
   heroes = signal(heroes);
-
 
   changeUpperCase() {
     if (this.upperCase()) {

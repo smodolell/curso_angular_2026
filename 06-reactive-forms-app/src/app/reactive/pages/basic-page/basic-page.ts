f0e35a0708ea86303fbe406fc52d7ef0055f3d18
Sergio Modolell <sergio.modolell@gmail.com>
2026-01-07 13:56:38 -0300

    184. Mostrar errores en pantalla

--- 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.ts
@@ -15,14 +15,33 @@ import {
 })
 export class BasicPage {
   private fb = inject(FormBuilder);
-  myForm = this.fb.group({
+
+  myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)], []],
     price: [0, [Validators.required, Validators.min(10)]],
-    inStorage: [0,[Validators.required,Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
   });
-  // myForm = new FormGroup({
-  //   name: new FormControl(''),
-  //   price: new FormControl(0),
-  //   isStorage: new FormControl(0),
-  // });
+
+  isValidField(fieldName: string): boolean | null {
+    return !!this.myForm.controls[fieldName].errors;
+  }
+  getFieldError(fieldName: string): string | null {
+    if (!this.myForm.controls[fieldName]) return null;
+
+    const errors = this.myForm.controls[fieldName].errors ?? {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Mínimo de ${errors['minlength'].requiredLength} caracteres.`;
+
+        case 'min':
+          return `Valor mínimo de ${errors['min'].min}`;
+      }
+    }
+     return null;
+  }
 }

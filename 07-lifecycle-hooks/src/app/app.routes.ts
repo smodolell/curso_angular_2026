8e144bbbd8d2806b38b447132eed6439541052cf
Sergio Modolell <sergio.modolell@gmail.com>
2026-01-08 00:00:54 -0300

    211. Inicio de proyecto - LifeCycle

---
+++ 07-lifecycle-hooks/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { Component } from '@angular/core';
+import { HomePage } from './pages/home-page/home-page';
+import { AboutPage } from './pages/about-page/about-page';
+import { ContactPage } from './pages/contact-page/contact-page';
+
+export const routes: Routes = [
+  { path: '', component: HomePage },
+  { path: 'about', component: AboutPage },
+  { path: 'contact', component: ContactPage },
+  { path: '**', redirectTo: '' },
+];

1fab099bb273a4c25079b29bebeca5bbed7dda00
Sergio Modolell <sergio.modolell@gmail.com>
2026-01-08 19:28:36 -0300

    224-No tengo Tarjeta de Credito no puedo sacar el apikey de maobox

--- 08-maps-app/src/app/pages/fullscreen-map-page/fullscreen-map-page.ts
+++ 08-maps-app/src/app/pages/fullscreen-map-page/fullscreen-map-page.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 @Component({
   selector: 'app-fullscreen-map-page',
   imports: [],
   templateUrl: './fullscreen-map-page.html',
+  styles: `
+    div {
+      width: 100vw;
+      height: calc( 100vh - 64px);
+    }
+
+    #controls {
+      background-color: white;
+      padding: 10px;
+      border-radius: 5px;
+      position: fixed;
+      bottom: 25px;
+      right: 20px;
+      z-index: 9999;
+      box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+      border: 1px solid #e2e8f0;
+      width: 250px;
+    }
+
+  `,
 })
 export class FullscreenMapPage { }

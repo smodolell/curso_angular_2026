591901a486d1d19f6235eec62337876b2a82a9a1
Sergio Modolell <sergio.modolell@gmail.com>
2026-01-07 22:26:11 -0300

    204. Suscribirse a los cambios del formulario

--- 06-reactive-forms-app/src/app/country/pages/country-page/country-page.ts
+++ 06-reactive-forms-app/src/app/country/pages/country-page/country-page.ts
@@ -1,8 +1,9 @@
 import { JsonPipe } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CountryService } from '../../services/country.service';
 import { Country } from '../../interfaces/country.interface';
+import { tap, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
@@ -21,4 +22,29 @@ export class CountryPage {
     country: ['', Validators.required],
     border: ['', Validators.required],
   });
+
+  onFormChanged = effect((onCleanup) => {
+    const regionSubscription = this.onRegionChanged();
+
+    onCleanup(() => {
+      regionSubscription.unsubscribe();
+    });
+  });
+
+  onRegionChanged() {
+    return this.myForm
+      .get('region')!
+      .valueChanges.pipe(
+        tap(() => this.myForm.get('country')!.setValue('')),
+        tap(() => this.myForm.get('border')!.setValue('')),
+        tap(() => {
+          this.borders.set([]);
+          this.countriesByRegion.set([]);
+        }),
+        switchMap((region) => this.countryService.getCountriesByRegion(region ?? ''))
+      )
+      .subscribe((countries) => {
+        this.countriesByRegion.set(countries);
+      });
+  }
 }

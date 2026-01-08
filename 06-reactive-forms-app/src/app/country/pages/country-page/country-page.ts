afe4341bcd59965c06db61fc0f50ecf056efcbaf
Sergio Modolell <sergio.modolell@gmail.com>
2026-01-07 21:51:23 -0300

    203. Primer selector controlado - Continentes

--- 06-reactive-forms-app/src/app/country/pages/country-page/country-page.ts
+++ 06-reactive-forms-app/src/app/country/pages/country-page/country-page.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './country-page.html',
 })
-export class CountryPage { }
+export class CountryPage {
+  private fb = inject(FormBuilder);
+  countryService = inject(CountryService);
+  regions = signal(this.countryService.regions);
+  countriesByRegion = signal<Country[]>([]);
+  borders = signal<Country[]>([]);
+
+  myForm = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    border: ['', Validators.required],
+  });
+}

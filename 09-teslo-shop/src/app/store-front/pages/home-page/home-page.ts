105310adcbd8b96df605c865e138f0e72bb5523f
Sergio Modolell <sergio.modolell@gmail.com>
2026-01-09 15:05:52 -0300

    245. Levantar backend - TesloShop

--- 09-teslo-shop/src/app/store-front/pages/home-page/home-page.ts
+++ 09-teslo-shop/src/app/store-front/pages/home-page/home-page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ProductCard } from "../../../products/components/product-card/product-card";
+import { ProductCard } from '@productos/components/product-card/product-card';
 
 @Component({
   selector: 'app-home-page',

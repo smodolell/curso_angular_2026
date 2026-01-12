bd72b72dc19974ab7f431fe57ba7872fad33e52a
Sergio Modolell <sergio.modolell@gmail.com>
2026-01-12 16:37:59 -0300

    253. Página de productos por género

--- 09-teslo-shop/src/app/store-front/pages/gender-page/gender-page.ts
+++ 09-teslo-shop/src/app/store-front/pages/gender-page/gender-page.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
+import { map } from 'rxjs';
+import { ProductCard } from "@products/components/product-card/product-card";
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './gender-page.html',
 })
-export class GenderPage { }
+export class GenderPage {
+  route = inject(ActivatedRoute);
+  productsService = inject(ProductsService);
+
+  gender = toSignal(this.route.params.pipe(map(({ gender }) => gender)));
+
+  productsResource = rxResource({
+    params: () => ({ gender: this.gender() }),
+    stream: ({ params }) => {
+      return this.productsService.getProducts({
+        gender: params.gender,
+      });
+    },
+  });
+}

da3ccac54b38618d8a1670ee01e2c19c06aaaff9
Sergio Modolell <sergio.modolell@gmail.com>
2026-01-13 15:20:53 -0300

    260. Paginación local - Funcionalidad

--- 09-teslo-shop/src/app/shared/components/pagination/pagination.ts
+++ 09-teslo-shop/src/app/shared/components/pagination/pagination.ts
@@ -1,13 +1,15 @@
-import { Component, computed, input, inputBinding } from '@angular/core';
+import { Component, computed, input, inputBinding, linkedSignal, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pagination',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pagination.html',
 })
 export class Pagination {
   pages = input(0);
   currentPage = input<number>(1);
+  activePage = linkedSignal(this.currentPage);
 
   getPageList = computed(() => {
     return Array.from({ length: this.pages() }, (_, i) => i + 1);

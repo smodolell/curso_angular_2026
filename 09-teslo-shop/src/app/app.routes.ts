b681ca35f255cdeb8edab81966fb8bdc11877ae6
Sergio Modolell <sergio.modolell@gmail.com>
2026-02-26 15:40:45 -0300

    280. Guards - notAuthenticatedGuard

--- 09-teslo-shop/src/app/app.routes.ts
+++ 09-teslo-shop/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
+import { NotAuthenticatedGuard } from '@auth/guards/not-authenticated.guard';
 
 export const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('@auth/auth.routes'),
+    canMatch:[NotAuthenticatedGuard]
   },
   {
     path: '',
